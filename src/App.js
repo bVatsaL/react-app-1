ef0e6d2ec2b85d807e4c0ddc408fd1d2712bb6d8
Vatsal Budhbhatti <vatsal@atyantik.com>
2023-03-17 04:36:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const API_URL = process.env.API_URL || 'https://app1-92ca.onrender.com';
+
+const App = () => {
+  const [itemName, setItemName] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isClearing, setIsClearing] = useState(false);
+
+  const handleItemSubmit = (e) => {
+    setIsSubmitting(true);
+    e?.preventDefault?.();
+    const data = { item: itemName };
+    fetch(`${API_URL}/api`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json', },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setItemName('');
+        setSuccessMessage(result.data);
+        setIsSubmitting(false);
+        setTimeout(() => {
+          setSuccessMessage('');
+        }, 3000);
+      });
+  };
+
+  const handleItemNameChange = (e) => setItemName(e.target.value);
+  const clearItems = () => {
+    setIsClearing(true);
+    fetch(`${API_URL}/api/clear`, {
+      method: 'DELETE',
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setIsClearing(false);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleItemSubmit}>
+          <div>
+            <div style={{ margin: '10px' }}>Create an item</div>
+            <input
+              className="inputStyle"
+              type="text"
+              name="itemName"
+              id="itemName"
+              value={itemName}
+              onChange={handleItemNameChange}
+            />
+            <button className="buttonStyle" type="submit" disabled={!itemName}>{isSubmitting ? 'Submitting...' : 'Submit'}</button>
+            <button className="buttonStyle" type="button" onClick={clearItems}>{isClearing ? 'Cleaning...' : 'Clear'}</button>
+          </div>
+        </form>
+        {!!successMessage && (
+          <span style={{margin: '10px'}}>{successMessage}</span>
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
